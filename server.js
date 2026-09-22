73fd20f9c80cc441adf98a4addac48d6b2a0f8c5
kelven00nharrass-prog <kelven00nharrass@gmail.com>
2026-09-22 19:07:10 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -58,7 +58,11 @@ const inMemoryOrders = new Map();
 // ----------------------------------------------------
 // 2. MIDDLEWARES
 // ----------------------------------------------------
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
